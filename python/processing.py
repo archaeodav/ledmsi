fbc999477cc9c93fb60f46bb460457d1db281603
David Stott <ds@moesgaardmuseum.dk>
2023-06-08 09:00:40 +0200

    Update processing.py

--- python/processing.py
+++ python/processing.py
@@ -33,6 +33,8 @@ from skimage.filters import threshold_otsu
 from skimage.filters import threshold_mean
 from skimage.filters import threshold_li
 
+from skimage import io
+
 class RGBimage():
     def __init__(self,
                  image):
@@ -295,7 +297,7 @@ class FluoStack(ImageDict):
              hstack = np.rot90(hstack,rotate)
              sstack = np.rot90(sstack,rotate)
              vstack = np.rot90(vstack,rotate)
-             himdiff = np.rot90(himdiff,rotate)
+             hdiff = np.rot90(hdiff,rotate)
 
           
          if save_stacks is True:
@@ -592,7 +594,32 @@ class Results(ArrayHandler):
     def save_plot(self):
         pass
     
-    
-    
-    
-    
+def run_comps(indir):
+    for d in os.listdir(indir):
+        if os.path.isdir(os.path.join(indir,d)):
+            a = ArrayHandler(indir, d)
+            stack = a.gen_image_stack_np(save_stack=False)
+            refl = a.bin_refl_composites(stack,r=[15,18,21])
+            fluo = a.fluo_comp(stack)
+            
+            reflc=np.dstack((equalize_hist(refl[1]),
+                            equalize_hist(refl[2]),
+                            equalize_hist(refl[3])))
+            
+            uvirc=np.dstack((equalize_hist(refl[0]),
+                            equalize_hist(refl[2]),
+                            equalize_hist(refl[4])))
+            
+            fluoc=np.dstack((equalize_hist(fluo[0]),
+                            equalize_hist(refl[2]),
+                            equalize_hist(fluo[4])))
+            
+            
+            io.imsave(os.path.join(indir,d+'_comp_rr_gg_bb'.tif), 
+                      reflc)
+            
+            io.imsave(os.path.join(indir,d+'_comp_rir_gg_buv'.tif), 
+                      uvirc)
+            
+            io.imsave(os.path.join(indir,d+'_comp_rrirf_gg_bbuvf'.tif), 
+                      fluoc)
